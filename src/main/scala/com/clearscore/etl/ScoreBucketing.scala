d4bc457387425c186e5a983e2915ee1793d8c480
Richard Jones <richardjones@quantexa.com>
2020-12-14 00:54:13 -0800

    fixing issues with score ranges

--- src/main/scala/com/clearscore/etl/ScoreBucketing.scala
+++ src/main/scala/com/clearscore/etl/ScoreBucketing.scala
@@ -4,16 +4,15 @@ import com.clearscore.spark.SparkScriptRunner
 import com.typesafe.config.Config
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{floor, udf, lit}
+import org.apache.spark.sql.functions.{ceil, lit, udf, upper}
 
 object ScoreBucketing extends SparkScriptRunner {
   val name = "ScoreBucketing"
 
-  // TODO: Unit test
   def formatNormalisedScore(score: Double, range: Double): String = {
-    val upperBound = (score +1 )*range
-    val lowerBound = if (score == 0.0) score else (score*range) + 1
-    if (upperBound == 0) "0-50" else s"$lowerBound-$upperBound"
+    val upperBound = if (score == 0) range else score*range
+    val lowerBound = if (upperBound == range) 0.0 else (upperBound - range) + 1
+    s"$lowerBound-$upperBound"
   }
 
   private val formatNormalisedScoreUdf = udf((x: Double, range: Double) => formatNormalisedScore(x, range))
@@ -29,7 +28,7 @@ object ScoreBucketing extends SparkScriptRunner {
     val reportsDf = spark.read.parquet(reportsPath)
     val idsAndScores = reportsDf
       .select($"user-uuid", $"Score")
-      .withColumn("normalised_rounded", floor($"Score"/scoreRange))
+      .withColumn("normalised_rounded", ceil($"Score"/scoreRange))
 
     val groupedAndFormatted = idsAndScores.groupBy("normalised_rounded").count()
       .withColumn("score-range", formatNormalisedScoreUdf($"normalised_rounded", lit(scoreRange)))
--- src/test/scala/com/clearscore/etl/ScoreBucketingTest.scala
+++ src/test/scala/com/clearscore/etl/ScoreBucketingTest.scala
@@ -6,15 +6,19 @@ import com.clearscore.etl.ScoreBucketing.formatNormalisedScore
 class ScoreBucketingTest extends FlatSpec with Matchers {
 
   "formatNormalisedScore" should "correctly format a given integer input for the score range it represents" in {
-    formatNormalisedScore(2, 50) shouldBe "101.0-150.0"
+    formatNormalisedScore(2, 50) shouldBe "51.0-100.0"
   }
 
   it should "correctly format a given integer for a non 50 score range" in {
-    formatNormalisedScore(3, 30) shouldBe "91.0-120.0"
+    formatNormalisedScore(3, 30) shouldBe "61.0-90.0"
   }
 
   it should "correctly format a 0 score input" in {
     formatNormalisedScore(0, 20) shouldBe "0.0-20.0"
+  }
+
+  it should "correctly format an input of 1" in {
+    formatNormalisedScore(1, 50) shouldBe "0.0-50.0"
   }
 
   // TODO: consider behaviour given a negative input (although we should never have a negative credit score)
